2ca406fc702da7b3e9c0aed89fba33a3e33c16c0
tomneo2004 <tomneo2004@gmail.com>
2023-08-15 13:08:10 +0800

    refactor(env): change server address
    change server address

--- next.config.js
+++ next.config.js
@@ -1,24 +1,18 @@
 module.exports = {
-    target: "serverless",
-    webpack : (config) => {
-        config.module.rules.push(
-            {
-                test: /\.(png|jpg|gif|svg)$/i,
-                exclude: [
-                    /\.inline.svg$/
-                ],
-                use:[
-                    {loader:'url-loader'}
-                ]
-            },
-            {
-                test: /\.inline.svg$/,
-                use: [
-                    {loader:'@svgr/webpack'}
-                ]
-            },
-        )
+  target: "serverless",
+  webpack: (config) => {
+    config.module.rules.push(
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        exclude: [/\.inline.svg$/],
+        use: [{ loader: "url-loader" }],
+      },
+      {
+        test: /\.inline.svg$/,
+        use: [{ loader: "@svgr/webpack" }],
+      }
+    );
 
-        return config;
-    }
+    return config;
+  },
 };
