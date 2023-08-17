700ab3091207150c29bcae5dfa202016bdee0548
tomneo2004 <tomneo2004@gmail.com>
2023-08-17 16:12:49 +0800

    fix: migrate to nextjs 13 and use Chakra UI and fix some issues
    migrate to nextjs 13 and use Chakra UI and fix some issues
    
    BREAKING CHANGE: project upgrade

--- src/components/concrete/Counter/Counter.tsx
+++ src/components/concrete/Counter/Counter.tsx
@@ -1,38 +1,31 @@
-import React from 'react';
-import { InView } from 'react-intersection-observer';
-import CountUp from 'react-countup';
-import Box, { BoxProps } from '@material-ui/core/Box/Box';
-import RootRef from '@material-ui/core/RootRef/RootRef';
-import Typography from '@material-ui/core/Typography/Typography';
+import React from "react";
+import { InView } from "react-intersection-observer";
+import CountUp from "react-countup";
+import { Text } from "@chakra-ui/react";
 
-type CounterProps = React.ComponentProps<typeof CountUp> & BoxProps & {
-    caption?: React.ReactNode,
-}
+type CounterProps = React.ComponentProps<typeof CountUp> & {
+  caption?: React.ReactNode;
+};
 
-const Counter:React.FC<CounterProps> = (props:CounterProps) => {
-    const {
-        caption,
-        ...rest
-    } = props;
+const Counter: React.FC<CounterProps> = (props: CounterProps) => {
+  const { caption, ...rest } = props;
 
-    return (
-        <CountUp {...rest}>
-        {({countUpRef, start})=>(
-            <InView
-            triggerOnce 
-            onChange={(inView)=>{
-                if(inView) start();
-            }}>
-                <Box display='flex' flexDirection='row' justifyItems='center' paddingX={2}>
-                    {caption}
-                    <div ref={countUpRef}>
-                        <Typography variant='inherit' align='center' />
-                    </div>
-                </Box>
-            </InView>
-        )}
-        </CountUp>
-    );
+  return (
+    <CountUp {...rest}>
+      {({ countUpRef, start }) => (
+        <InView
+          triggerOnce
+          onChange={(inView) => {
+            if (inView) start();
+          }}
+        >
+          <Text fontSize="3xl" fontWeight="bold" ref={countUpRef}>
+            {caption}
+          </Text>
+        </InView>
+      )}
+    </CountUp>
+  );
 };
 
 export default Counter;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,28 +1,43 @@
-import {createStore, Store, applyMiddleware, combineReducers} from 'redux';
-import thunk from 'redux-thunk';
-import {createWrapper, Context} from 'next-redux-wrapper';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { wineReducer, WineState } from './reducers/wineReducer';
-
-
+import {
+  //   legacy_createStore as createStore,
+  Store,
+  //   applyMiddleware,
+  combineReducers,
+} from "redux";
+import { configureStore } from "@reduxjs/toolkit";
+// import thunk from "redux-thunk";
+import { createWrapper, Context } from "next-redux-wrapper";
+// import { composeWithDevTools } from '@redux-devtools/extension';
+import { wineReducer } from "./reducers/wineReducer";
 
 // Create middleware
-const middleware = composeWithDevTools(applyMiddleware(...[thunk]));
-
-//Root state
-export type RootState = {
-    wine: WineState,
-};
+// const middleware = composeWithDevTools(applyMiddleware(...[thunk]));
 
 // Combine reducers
-const rootReducer = combineReducers<RootState>({
-    wine: wineReducer,
+// const rootReducer = combineReducers<RootState>({
+//   wine: wineReducer,
+// });
+const rootReducer = {
+  wine: wineReducer,
+};
+
+// Redux store
+export const store = configureStore({
+  reducer: rootReducer,
 });
 
-// Create a make store function
-const makeStore = (context: Context)=>createStore(rootReducer, middleware);
+// Root state type
+// export type RootState = {
+//     wine: WineState;
+//   };
+export type RootState = ReturnType<typeof store.getState>;
+// Dispatch type
+export type AppDispatch = typeof store.dispatch;
 
-const wrapper = createWrapper<Store<RootState>>(makeStore, {debug:true});
+// Create a make store function
+const makeStore = (context: Context) => store;
+// const makeStore = (context: Context) => createStore(rootReducer, middleware);
+
+const wrapper = createWrapper<Store<RootState>>(makeStore, { debug: true });
 
 export default wrapper;
-
